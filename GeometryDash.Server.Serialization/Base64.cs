268afcec9b659d68c9fb6dc36298cedaa3d019b8
xparadoxical <43796685+xparadoxical@users.noreply.github.com>
2023-12-11 15:33:55 +0100

    Add base64 decoding

---
+++ GeometryDash.Server.Serialization/Base64.cs
@@ -0,0 +1,43 @@
+using System.Buffers;
+using System.Text;
+
+using Base64Lib = gfoidl.Base64.Base64;
+
+namespace GeometryDash.Server.Serialization;
+public static class Base64
+{
+    internal const int StackallocTreshold = 512;
+
+    public static string Decode(ReadOnlySpan<byte> base64Bytes)
+    {
+        base64Bytes = base64Bytes.TrimEnd((byte)'=');
+
+        byte[]? decodedArray = null;
+        scoped Span<byte> decoded;
+
+        var decodedLength = Base64Lib.Url.GetMaxDecodedLength(base64Bytes.Length);
+        if (decodedLength > StackallocTreshold)
+        {
+            decodedArray = ArrayPool<byte>.Shared.Rent(decodedLength);
+            decoded = decodedArray.AsSpan();
+        }
+        else
+            decoded = stackalloc byte[StackallocTreshold];
+
+        var status = Base64Lib.Url.Decode(base64Bytes, decoded, out _, out var written);
+
+        if (status != OperationStatus.Done)
+        {
+            if (decodedArray is not null)
+                ArrayPool<byte>.Shared.Return(decodedArray);
+            ThrowHelpers.OperationStatusUnsuccessful(status);
+        }
+
+        var str = Encoding.UTF8.GetString(decoded[..written]);
+
+        if (decodedArray is not null)
+            ArrayPool<byte>.Shared.Return(decodedArray);
+
+        return str;
+    }
+}
