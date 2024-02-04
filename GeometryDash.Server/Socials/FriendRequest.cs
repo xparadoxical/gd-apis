606147217e9a0e2a75bf54bf7bc521d80fe06d4d
xparadoxical <43796685+xparadoxical@users.noreply.github.com>
2024-02-04 21:46:32 +0100

    Add FriendRequest

---
+++ GeometryDash.Server/Socials/FriendRequest.cs
@@ -0,0 +1,22 @@
+using GeometryDash.Server.Users;
+
+namespace GeometryDash.Server.Socials;
+public class FriendRequest
+{
+    protected internal FriendRequest(UserResponse response)
+    {
+        OtherUser = new(response);
+
+        Id = response.FriendRequestId!.Value;
+        Message = response.FriendRequestMessage!;
+        Age = response.FriendRequestAge!.Value;
+        IsUnread = response.IsNewFriendOrRequest!.Value;
+    }
+
+    public PreviewedUser OtherUser { get; set; }
+
+    public uint Id { get; set; }
+    public string Message { get; set; }
+    public TimeSpan Age { get; set; }
+    public bool IsUnread { get; set; }
+}
