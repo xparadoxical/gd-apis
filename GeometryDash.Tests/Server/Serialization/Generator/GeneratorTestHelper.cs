77635aaa1cdead14cf9ea60dbed218caceec2e18
xparadoxical <43796685+xparadoxical@users.noreply.github.com>
2025-02-16 19:37:43 +0100

    Return compilation info to tests

--- GeometryDash.Tests/Server/Serialization/Generator/GeneratorTestHelper.cs
+++ GeometryDash.Tests/Server/Serialization/Generator/GeneratorTestHelper.cs
@@ -8,28 +8,33 @@ using Microsoft.CodeAnalysis.CSharp;
 namespace GeometryDash.Tests.Server.Serialization.Generator;
 public static class GeneratorTestHelper
 {
-    public static GeneratorDriver CreateDriverForSource(string source)
+    public sealed record GeneratorData(GeneratorDriver Driver, Compilation Input, Compilation Output);
+
+    public static GeneratorData CreateDriverForSource(string source)
     {
-        var compilation = CSharpCompilation.Create(
+        var inputCompilation = CSharpCompilation.Create(
             "Tests",
             [CSharpSyntaxTree.ParseText(source)],
             [
                 .. Basic.Reference.Assemblies.Net80.References.All,
                 MetadataReference.CreateFromFile(typeof(ServerSerializer).Assembly.Location)
-            ]
+            ],
+            new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary, allowUnsafe: true)
         );
 
-        return CSharpGeneratorDriver.Create(new SerializerGenerator())
-            .RunGenerators(compilation);
+        var driver = CSharpGeneratorDriver.Create([new SerializerGenerator().AsSourceGenerator()],
+            parseOptions: new CSharpParseOptions())
+            .RunGeneratorsAndUpdateCompilation(inputCompilation, out var outputCompilation, out _);
+        return new(driver, inputCompilation, outputCompilation);
     }
 
-    public static GeneratorDriver CreateDriverFromFile(string fileName)
+    public static GeneratorData CreateDriverFromFile(string fileName)
     {
         var source = File.ReadAllText(Path.Combine(Constants.SourceFolderFullPath, fileName));
         return CreateDriverForSource(source);
     }
 
-    public static GeneratorDriver CreateDriverForTest(
+    public static GeneratorData CreateDriverForTest(
         [CallerFilePath] string callerFilePath = "",
         [CallerMemberName] string callerMemberName = "")
     {
