6a4526825d51edcf99f5e066956708c0fbd9dce9
xparadoxical <43796685+xparadoxical@users.noreply.github.com>
2025-03-12 01:00:53 +0100

    FAWMN doesn't like `global::`

--- GeometryDash.Server.Serialization.Generator/KnownTypes.cs
+++ GeometryDash.Server.Serialization.Generator/KnownTypes.cs
@@ -2,18 +2,18 @@ namespace GeometryDash.Server.Serialization.Generator;
 
 internal static class KnownTypes
 {
-    public const string SeparatorAttribute = "global::GeometryDash.Server.Serialization.SeparatorAttribute";
-    public const string KeyedAttribute = "global::GeometryDash.Server.Serialization.KeyedAttribute";
+    public const string SeparatorAttribute = "GeometryDash.Server.Serialization.SeparatorAttribute";
+    public const string KeyedAttribute = "GeometryDash.Server.Serialization.KeyedAttribute";
 
-    public const string IndexAttribute = "global::GeometryDash.Server.Serialization.IndexAttribute";
-    public const string BoolAttribute = "global::GeometryDash.Server.Serialization.BoolAttribute";
+    public const string IndexAttribute = "GeometryDash.Server.Serialization.IndexAttribute";
+    public const string BoolAttribute = "GeometryDash.Server.Serialization.BoolAttribute";
 
-    public const string CoalesceToNullAttribute = "global::GeometryDash.Server.Serialization.CoalesceToNullAttribute";
-    public const string EmptyDefaultsToAttribute = "global::GeometryDash.Server.Serialization.EmptyDefaultsToAttribute";
+    public const string CoalesceToNullAttribute = "GeometryDash.Server.Serialization.CoalesceToNullAttribute";
+    public const string EmptyDefaultsToAttribute = "GeometryDash.Server.Serialization.EmptyDefaultsToAttribute";
 
-    public const string Base64EncodedAttribute = "global::GeometryDash.Server.Serialization.Base64EncodedAttribute";
-    public const string XorAttribute = "global::GeometryDash.Server.Serialization.XorAttribute";
-    public const string GzipAttribute = "global::GeometryDash.Server.Serialization.GzipAttribute";
+    public const string Base64EncodedAttribute = "GeometryDash.Server.Serialization.Base64EncodedAttribute";
+    public const string XorAttribute = "GeometryDash.Server.Serialization.XorAttribute";
+    public const string GzipAttribute = "GeometryDash.Server.Serialization.GzipAttribute";
 
-    public const string ISerializable = "global::GeometryDash.Server.Serialization.ISerializable";
+    public const string ISerializable = "GeometryDash.Server.Serialization.ISerializable";
 }
