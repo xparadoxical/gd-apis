65ff62c8826deb6470192455f648a8912c1367b0
xparadoxical <43796685+xparadoxical@users.noreply.github.com>
2025-09-28 23:17:48 +0200

    Add TestArrayDeserialization

--- GeometryDash.Tests/SerializationTest.cs
+++ GeometryDash.Tests/SerializationTest.cs
@@ -7,4 +7,12 @@ public class SerializationTest
             .Select(inOut => new Action(() => Assert.Equivalent(inOut.Second, ServerSerializer.DeserializeSerializable<T>(inOut.First), true)))
             .ToArray());
     }
+
+    protected void TestArrayDeserialization<T>(byte[] input, T[] expectedOutputs) where T : ISerializable<T>
+    {
+        Assert.Multiple(ServerSerializer.DeserializeArray<T>(input)
+            .Zip(expectedOutputs)
+            .Select(inOut => new Action(() => Assert.Equivalent(inOut.Second, inOut.First, true)))
+            .ToArray());
+    }
 }
