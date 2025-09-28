2f703ac87fb24a624e1f7a0d8589a1fabb6d9841
xparadoxical <43796685+xparadoxical@users.noreply.github.com>
2025-09-28 20:00:08 +0200

    forgot strict mode

--- GeometryDash.Tests/SerializationTest.cs
+++ GeometryDash.Tests/SerializationTest.cs
@@ -4,7 +4,7 @@ public class SerializationTest
     protected void TestDeserialization<T>(byte[][] inputs, T[] expectedOutputs) where T : ISerializable<T>
     {
         Assert.Multiple(inputs.Zip(expectedOutputs)
-            .Select(inOut => new Action(() => Assert.Equivalent(inOut.Item2, ServerSerializer.DeserializeSerializable<T>(inOut.Item1))))
+            .Select(inOut => new Action(() => Assert.Equivalent(inOut.Item2, ServerSerializer.DeserializeSerializable<T>(inOut.Item1), true)))
             .ToArray());
     }
 }
