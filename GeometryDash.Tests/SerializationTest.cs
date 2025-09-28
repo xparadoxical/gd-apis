e0cdbdfd938b1bd06f51542a261e471430581272
xparadoxical <43796685+xparadoxical@users.noreply.github.com>
2025-09-28 20:02:45 +0200

    also use friendly tuple member names

--- GeometryDash.Tests/SerializationTest.cs
+++ GeometryDash.Tests/SerializationTest.cs
@@ -4,7 +4,7 @@ public class SerializationTest
     protected void TestDeserialization<T>(byte[][] inputs, T[] expectedOutputs) where T : ISerializable<T>
     {
         Assert.Multiple(inputs.Zip(expectedOutputs)
-            .Select(inOut => new Action(() => Assert.Equivalent(inOut.Item2, ServerSerializer.DeserializeSerializable<T>(inOut.Item1), true)))
+            .Select(inOut => new Action(() => Assert.Equivalent(inOut.Second, ServerSerializer.DeserializeSerializable<T>(inOut.First), true)))
             .ToArray());
     }
 }
