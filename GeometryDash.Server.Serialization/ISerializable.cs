d9aa39a1a735ff6ec1f5d3bcb0f89294339ae1ff
xparadoxical <43796685+xparadoxical@users.noreply.github.com>
2025-09-30 19:53:06 +0200

    Make Deserialize a SAII

--- GeometryDash.Server.Serialization/ISerializable.cs
+++ GeometryDash.Server.Serialization/ISerializable.cs
@@ -2,12 +2,9 @@ using System.ComponentModel;
 
 namespace GeometryDash.Server.Serialization;
 
-//TODO replace with a source generator
-/// <summary>
-/// Provides reflection-less logic for serializing.
-/// </summary>
+/// <summary>Provides serialization logic.</summary>
 public interface ISerializable<TSelf> where TSelf : ISerializable<TSelf>
 {
     [EditorBrowsable(EditorBrowsableState.Never)]
-    public static virtual TSelf Deserialize(ReadOnlySpan<byte> input) => throw new NotImplementedException(); //TODO remove body when SG will be usable
+    public static abstract TSelf Deserialize(ReadOnlySpan<byte> input);
 }
