1bc62347b2c34389badb26cabf876a27175ff151
xparadoxical <43796685+xparadoxical@users.noreply.github.com>
2025-09-30 18:23:44 +0200

    Add single-output TestDeserialization

--- GeometryDash.Tests/SerializationTest.cs
+++ GeometryDash.Tests/SerializationTest.cs
@@ -1,10 +1,13 @@
 namespace GeometryDash.Tests;
 public class SerializationTest
 {
+    protected void TestDeserialization<T>(byte[] input, T expectedOutput) where T : ISerializable<T>
+        => Assert.Equivalent(expectedOutput, ServerSerializer.DeserializeSerializable<T>(input), true);
+
     protected void TestDeserialization<T>(byte[][] inputs, T[] expectedOutputs) where T : ISerializable<T>
     {
         Assert.Multiple(inputs.Zip(expectedOutputs)
-            .Select(inOut => new Action(() => Assert.Equivalent(inOut.Second, ServerSerializer.DeserializeSerializable<T>(inOut.First), true)))
+            .Select(inOut => new Action(() => TestDeserialization(inOut.First, inOut.Second)))
             .ToArray());
     }
 
