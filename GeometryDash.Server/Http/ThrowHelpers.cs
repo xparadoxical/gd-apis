a2193847362068d54086b748e73262de9ec1e0b2
xparadoxical <43796685+xparadoxical@users.noreply.github.com>
2026-06-03 23:11:01 +0200

    remove unused ThrowHelpers

--- GeometryDash.Server/Http/ThrowHelpers.cs
+++
@@ -1,6 +0,0 @@
-namespace GeometryDash.Server.Http;
-internal static class ThrowHelpers
-{
-    internal static void ThrowFormatProviderNotSupported()
-        => throw new NotSupportedException("IFormatProvider is not supported");
-}
