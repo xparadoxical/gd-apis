aa958dd1db5e5b8d9f0439fb7d5e742c69e6b663
xparadoxical <43796685+xparadoxical@users.noreply.github.com>
2026-06-06 23:37:44 +0200

    format

--- GeometryDash.Server.Serialization/ServerSerializer.cs
+++ GeometryDash.Server.Serialization/ServerSerializer.cs
@@ -1,9 +1,11 @@
 namespace GeometryDash.Server.Serialization;
 public sealed partial class ServerSerializer
 {
-    public static T DeserializeSerializable<T>(ReadOnlySpan<byte> input, SerializationContext? context = null) where T : ISerializable<T>
+    public static T DeserializeSerializable<T>(ReadOnlySpan<byte> input, SerializationContext? context = null)
+        where T : ISerializable<T>
         => T.Deserialize(input, context);
 
-    public static T[] DeserializeArray<T>(ReadOnlySpan<byte> input, SerializationContext? context = null) where T : ISerializable<T>
+    public static T[] DeserializeArray<T>(ReadOnlySpan<byte> input, SerializationContext? context = null)
+        where T : ISerializable<T>
         => T.DeserializeArray(input, context);
 }
